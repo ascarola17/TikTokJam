ea92b5fd0398908166e4809c79e270462fd21813
dyllanesl <dyllaneslava@gmail.com>
2024-07-05 23:10:21 -0400

    Update

--- tester.py
+++
@@ -1,3 +0,0 @@
-your_list = ['', 'mobile gamepad', 'game controller', 'gamepad', 'google stadia', 'google stadia wasabi edition controller limited edition open box', 'emulator']
-single_string = ' '.join(your_list)
-print(single_string)
