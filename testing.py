ac787dc1fccbb7410dd4d7ffa1233e6df769a648
rauugom <romargomez04@gmail.com>
2024-07-05 21:56:52 -0400

    updated Google API guy
    yayaayayayay

--- testing.py
+++ testing.py
@@ -7,8 +7,7 @@ from dotenv import load_dotenv, dotenv_values
 
 # Set the environment variable ??
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:\\Users\\Admin\\Desktop\\AI_EDGE\\snap-market-428419-cf3dcb6ba810.json"
-
-def closest_color(requested_color):
+def closest_color( requested_color):
     min_colors = {}
     for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
@@ -18,7 +17,7 @@ def closest_color(requested_color):
         min_colors[(rd + gd + bd)] = name
     return min_colors[min(min_colors.keys())]
 
-def get_color_name(rgb_color):
+def get_color_name( rgb_color):
     try:
         hex_code = webcolors.rgb_to_hex((int(rgb_color.red), int(rgb_color.green), int(rgb_color.blue)))
         closest_name = webcolors.hex_to_name(hex_code)
@@ -26,9 +25,9 @@ def get_color_name(rgb_color):
         closest_name = closest_color(rgb_color)
     return closest_name
 
-def analyze_image(image_path):
-    #client = vision_v1.ImageAnnotatorClient()
-  
+def analyze_image( image_path):
+#client = vision_v1.ImageAnnotatorClient()
+
 
     # Initialize the client with ClientOptions
     client = vision_v1.ImageAnnotatorClient()
@@ -42,6 +41,7 @@ def analyze_image(image_path):
         vision_v1.Feature(type_=vision_v1.Feature.Type.LOGO_DETECTION),
         vision_v1.Feature(type_=vision_v1.Feature.Type.WEB_DETECTION),
         vision_v1.Feature(type_=vision_v1.Feature.Type.IMAGE_PROPERTIES),
+        vision_v1.Feature(type_=vision_v1.Feature.Type.TEXT_DETECTION),
     ]
     request = vision_v1.AnnotateImageRequest(image=image, features=features)
 
@@ -51,36 +51,50 @@ def analyze_image(image_path):
     # Retrieve annotations
     logos = [logo.description for logo in response.responses[0].logo_annotations]
     web_entities = [entity.description for entity in response.responses[0].web_detection.web_entities]
-    colors = [color.color for color in response.responses[0].image_properties_annotation.dominant_colors.colors]
+    #colors = [color.color for color in response.responses[0].image_properties_annotation.dominant_colors.colors]
+    texts = [text.description for text in response.responses[0].text_annotations]
 
 
-    color_names = [get_color_name(color) for color in colors]
+    print("text caption")
+    print(texts)
+    #color_names = [get_color_name(color) for color in colors]
     
     caption = []
-   
+
     caption.extend(logos)
     caption.extend(web_entities)
-    caption.extend(color_names)
-    print("Junior thing")
+    caption.extend(texts)
+    
     #vv removing duplicates
     caption = list(set(caption))
-
     print(caption)
-    #posting to Jr API 
-    #make a post method
-    json_response = json.dumps({
-        "caption": caption
-    })
-    print("this is the jso0n:")
+    word_counts = {}
+    for word in caption:
+        word = word.lower()
+        if word in word_counts:
+            word_counts[word]+=1
+        else:
+            word_counts[word] = 1
+
+    sorted_counts = dict(sorted(word_counts.items(), key=lambda item: item[1], reverse=True))
+    sorted_counts = list(sorted_counts.keys())[:7] 
+    nice_sentence = ' '.join(sorted_counts)
+
+    json_response = {
+        #"caption": caption
+        "caption": nice_sentence # Get top 5
+    }
     print(json_response)
+    #make post
+     
     #api call  
-    # post_data = {'name': 'Gladys'}
-    # response = requests.post('http://.com', data=post_data)
+    
+    response = requests.post('http://localhost:8000/serpapi_search/', data=json_response)
+    print(response)
     # content = response.content
     #response = requests.post(url, data=data)
 
 
-
 # Provide the path to your local image file
-image_path = "C:\\Users\\Admin\\Desktop\\AI_EDGE\\tester_img.png"
+image_path = "C:\\Users\\Admin\\Desktop\\AI_EDGE\\silly.png"
 analyze_image(image_path)
--- tiktokjam/mytiktok/views.py
+++ tiktokjam/mytiktok/views.py
@@ -14,6 +14,12 @@ from google.cloud import vision
 from django.views.decorators.csrf import csrf_exempt
 from . import views
 from django.views import View
+#raul imports
+from google.cloud import vision_v1
+import webcolors
+import json
+import requests
+from transformers import pipeline
 
 
 class SerpApiSearchView(APIView):
@@ -26,6 +32,7 @@ class SerpApiSearchView(APIView):
         try:
             serp_api = SerpApi(api_key=os.environ["SERPAPI"])
             search_results = serp_api.search_google(caption)
+            print(search_results)
             return Response({"search_results": search_results}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -53,7 +60,7 @@ class ImageUploadViewSet(viewsets.ViewSet):
             #print(serializer)
             
             # Ensure the temp directory exists
-            temp_dir = os.path.join('media') #may remove temp
+            temp_dir = os.path.join('media') 
             if not os.path.exists(temp_dir):
                 os.makedirs(temp_dir)
             
@@ -62,7 +69,108 @@ class ImageUploadViewSet(viewsets.ViewSet):
             with open(image_path, 'wb+') as destination:
                 for chunk in image.chunks():
                     destination.write(chunk)
-
+            
+            print("before analyze img")
+            analyze_image(image_path)
+            print("after")
+            
             return Response({"message": "Image uploaded successfully"}, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+"""
+Description: Helper methods for the webcolors 1.11.1 methods. These will allow for color descriptors 
+    closest_colors provides the neareset colors in hex
+    get_color_name obtains the english color name
+"""
+def closest_color( requested_color):
+    min_colors = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_color.red) ** 2
+        gd = (g_c - requested_color.green) ** 2
+        bd = (b_c - requested_color.blue) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
+
+def get_color_name( rgb_color):
+    try:
+        hex_code = webcolors.rgb_to_hex((int(rgb_color.red), int(rgb_color.green), int(rgb_color.blue)))
+        closest_name = webcolors.hex_to_name(hex_code)
+    except ValueError:
+        closest_name = closest_color(rgb_color)
+    return closest_name
+
+"""
+Description: analyze_image takes in an image and utilizies the Google Cloud API and certain features to obtain key words and descriptors.
+    It will then return a json for the serpapi to use.
+"""
+def analyze_image( image_path):
+
+    # Initialize the client with ClientOptions
+    client = vision_v1.ImageAnnotatorClient()
+
+    with open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    # Create an AnnotateImageRequest object
+    image = vision_v1.Image(content=content)
+    features = [
+        vision_v1.Feature(type_=vision_v1.Feature.Type.LOGO_DETECTION),
+        vision_v1.Feature(type_=vision_v1.Feature.Type.WEB_DETECTION),
+        vision_v1.Feature(type_=vision_v1.Feature.Type.IMAGE_PROPERTIES),
+        vision_v1.Feature(type_=vision_v1.Feature.Type.TEXT_DETECTION),
+    ]
+    request = vision_v1.AnnotateImageRequest(image=image, features=features)
+
+    # Make the API call using batch_annotate_images()
+    response = client.batch_annotate_images(requests=[request])
+
+    # Retrieve annotations
+    logos = [logo.description for logo in response.responses[0].logo_annotations]
+    web_entities = [entity.description for entity in response.responses[0].web_detection.web_entities]
+    #colors = [color.color for color in response.responses[0].image_properties_annotation.dominant_colors.colors]
+    texts = [text.description for text in response.responses[0].text_annotations]
+
+
+    print("text caption")
+    print(texts)
+    #color_names = [get_color_name(color) for color in colors]
+    
+    caption = []
+
+    caption.extend(logos)
+    caption.extend(web_entities)
+    caption.extend(texts)
+    
+    #vv removing duplicates
+    caption = list(set(caption))
+    print(caption)
+    word_counts = {}
+    for word in caption:
+        word = word.lower()
+        if word in word_counts:
+            word_counts[word]+=1
+        else:
+            word_counts[word] = 1
+
+    sorted_counts = dict(sorted(word_counts.items(), key=lambda item: item[1], reverse=True))
+    sorted_counts = list(sorted_counts.keys())[:7] 
+    nice_sentence = ' '.join(sorted_counts)
+
+    json_response = {
+        #"caption": caption
+        "caption": nice_sentence # Get top 5
+    }
+    print(json_response)
+    #make post
+     
+    #api call  
+    
+    response = requests.post('http://localhost:8000/serpapi_search/', data=json_response)
+    print(response)
+    # content = response.content
+    #response = requests.post(url, data=data)
+
